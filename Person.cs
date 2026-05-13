5b2233276f3c8e7909ac67d2d08757f1c4d2a7cd
HenryElendheim <henskielendheim@gmail.com>
2026-05-13 12:34:09 +0200

    Finalized

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParProg1
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+
+        public Person(string name, int age, string hobby)
+        {
+            Name = name;
+            Age = age;
+            Hobby = hobby;
+        }
+
+        public void ReadInfo()
+        {
+            Console.WriteLine($"Name: {Name} | Age: {Age} | Hobby: {Hobby}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿
-
-
-
-
-using ParProg1;
+﻿using ParProg1;
 using System.Net.Cache;
 
-var celine = new Celine();
 
 
-celine.MainMethod();
+Console.WriteLine("Here you can write a person's name, age and hobby!");
+var inputName = Console.ReadLine();
+var inputAge = Convert.ToInt32(Console.ReadLine());
+var inputHobby = Console.ReadLine();
+
+var person = new Person(inputName, inputAge, inputHobby);
+
+person.ReadInfo();
+
+
+
+//var henry = new Person("Henry", 20, "Coding, tegning, musikk og gaming");
+//var celine = new Person("Celine", 27, "Gaming, musikk, sang og kvalitetstid med venner");
