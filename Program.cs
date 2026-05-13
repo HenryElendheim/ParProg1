6a24759a98b69e30b835a1c979d50c66c7edb32f
HenryElendheim <henskielendheim@gmail.com>
2026-05-13 12:48:22 +0200

    Finalized Again

--- Program.cs
+++ Program.cs
@@ -1,18 +1,16 @@
 ﻿using ParProg1;
 using System.Net.Cache;
 
-
-
-Console.WriteLine("Here you can write a person's name, age and hobby!");
+Console.WriteLine("Here you can write a person's name, age and hobby! ");
 var inputName = Console.ReadLine();
 var inputAge = Convert.ToInt32(Console.ReadLine());
 var inputHobby = Console.ReadLine();
 
 var person = new Person(inputName, inputAge, inputHobby);
 
+var person1 = new Person("Henry", 20, "koding, tegning, musikk og gaming");
+var person2 = new Person("Celine", 27, "gaming, musikk, sang og kvalitetstid med venner");
+
 person.ReadInfo();
-
-
-
-//var henry = new Person("Henry", 20, "Coding, tegning, musikk og gaming");
-//var celine = new Person("Celine", 27, "Gaming, musikk, sang og kvalitetstid med venner");
+Console.WriteLine($"\nHei jeg heter, {person1.Name} og er {person1.Age} år. jeg liker {person1.Hobby}.\n" +
+                  $"Hei, jeg heter {person2.Name}, jeg er {person2.Age} år gammel, og på fritiden så liker jeg {person2.Hobby}.");
